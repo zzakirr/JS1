8479bd5d4d45f0be84d14271c03017c7b0d588c3
zzakirr <7nez6ws@code.edu.az>
2022-05-11 15:27:04 +0400

    Created File

---
+++ JS/main.js
@@ -0,0 +1,45 @@
+// Task1
+// let arr=[1, 2, 3, 4, 5 ];
+// function MaxNum(arr)
+// {
+// let max =arr[0];
+// for (let i = 0; i < arr.length; i++) {
+// if(arr[i]>max){
+//     max=arr[i];
+// }    
+// }
+// return max;
+// }
+// var maxnum = MaxNum(arr);
+// alert(maxnum);
+
+// Task2
+// let arr=[1,2,3,4,5,6,7]
+// function SumEven(arr)
+// {
+//     let Sum=0;
+//     for (let i = 0; i < arr.length; i++) {
+        
+//         if(arr[i]%2==0){
+//             Sum+=arr[i];
+//         }
+
+//     }
+//     return Sum;
+// }
+// var sumeven = SumEven(arr);
+// alert(sumeven);
+
+// Task3
+// var num1= prompt("eded 1:");
+// var num2 = prompt("eded 2:");
+// function Power(num1,num2){
+//     var temp=num2;
+//     while(num1>1){
+//         num2*=temp;
+//         num1-=1;
+//     }
+//     return num2;
+// }
+// var power=Power(num1,num2);
+// alert(power);
